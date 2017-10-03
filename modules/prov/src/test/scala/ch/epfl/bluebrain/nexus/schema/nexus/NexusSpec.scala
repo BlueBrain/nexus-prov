db8863743b86571f6e7a10d96f92c61485b8738c
Bogdan Roman <bogdanromanx@gmail.com>
2017-10-03 14:01:52 +0200

    Use sbt-nexus plugin for build / publish (#7)
    * Added sbt jvm opts for the project
    
    * Added scala formatting configuration
    
    * Removed orphaned script file
    
    * Migrated to `sbt-nexus-0.4.1`
    
    Also formatted the code according to the scalafmt rules.

--- modules/prov/src/test/scala/ch/epfl/bluebrain/nexus/schema/nexus/NexusSpec.scala
+++ modules/prov/src/test/scala/ch/epfl/bluebrain/nexus/schema/nexus/NexusSpec.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.model.Uri
 import akka.http.scaladsl.model.Uri.Path
 import cats.instances.try_._
 import ch.epfl.bluebrain.nexus.commons.shacl.validator.ShaclValidator
-import ch.epfl.bluebrain.nexus.schema.BuildInfo
+import ch.epfl.bluebrain.nexus.prov.BuildInfo
 import ch.epfl.bluebrain.nexus.schema.workbench.{ClasspathResolver, ResourceLoader, SchemaSpec}
 import org.scalatest.Suites
 
@@ -12,8 +12,8 @@ import scala.util.Try
 
 class NexusSpec extends Suites {
 
-  private val loader = new ResourceLoader[Try](Uri(BuildInfo.base), Map("{{base}}" -> BuildInfo.base))
-  private val resolver = new ClasspathResolver[Try](loader)
+  private val loader    = new ResourceLoader[Try](Uri(BuildInfo.base), Map("{{base}}" -> BuildInfo.base))
+  private val resolver  = new ClasspathResolver[Try](loader)
   private val validator = ShaclValidator(resolver)
 
   private val schemas = BuildInfo.schemas.foldLeft(Vector.empty[Uri]) {
@@ -28,12 +28,11 @@ class NexusSpec extends Suites {
     case (acc, e) => s"""${BuildInfo.base}${e.substring(0, e.lastIndexOf(".json"))}""" :: acc
   }
 
-  override val nestedSuites = schemas.map {
-    schemaUri =>
-      val id = schemaId(schemaUri)
-      val validUris = valid.filter(_.toString.startsWith(s"${BuildInfo.base}/data/$id"))
-      val invalidUris = invalid.filter(_.toString.startsWith(s"${BuildInfo.base}/invalid/$id"))
-      new SchemaSpec(validator, loader, Uri(BuildInfo.base), schemaUri, validUris, invalidUris)
+  override val nestedSuites = schemas.map { schemaUri =>
+    val id          = schemaId(schemaUri)
+    val validUris   = valid.filter(_.toString.startsWith(s"${BuildInfo.base}/data/$id"))
+    val invalidUris = invalid.filter(_.toString.startsWith(s"${BuildInfo.base}/invalid/$id"))
+    new SchemaSpec(validator, loader, Uri(BuildInfo.base), schemaUri, validUris, invalidUris)
   }
 
   private def schemaId(schemaUri: Uri): Path =
